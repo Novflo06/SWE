a510d2026470508a9cc56e9e2deb61248dbd104b
Novflo06 <florian.novosel@edu.campus02.at>
2021-10-22 16:59:48 +0200

    Modulo Rechnung hinzugefuegt.

--- stackCalculator/src/main/java/at/edu/c02/calculator/logic/CalculatorImpl.java
+++ stackCalculator/src/main/java/at/edu/c02/calculator/logic/CalculatorImpl.java
@@ -28,6 +28,11 @@ public class CalculatorImpl implements Calculator {
 			return c;
 		case mul:
 			return a * b;
+			case mod:
+				double modresult = a%b;
+				if(Double.isInfinite(modresult))
+					throw new CalculatorException("Modulo division by zero");
+				return modresult;
 		}
 		return 0;
 	}
